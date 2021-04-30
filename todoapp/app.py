f2abc3299c571bb681f5c5e82b2e9527f688790e
Siwen Wang <wendydoog@gmail.com>
2021-04-30 18:03:52 -0500

    update todoapp: migration control

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for,jsonify,abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://siwenwang@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app,db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String(), nullable = False)
-
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+#db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
