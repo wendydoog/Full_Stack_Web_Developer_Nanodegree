8303055d5913a673690797786b10c868b1bc06d8
Siwen Wang <wendydoog@gmail.com>
2021-04-30 21:58:07 -0500

    update todoapp: finish deleting function

--- todoapp/app.py
+++ todoapp/app.py
@@ -41,8 +41,37 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo=Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/todos/<todo_id>', methods = ['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
+
+
+
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', data = Todo.query.all()
+    return render_template('index.html', data = Todo.query.order_by('id').all()
     )
