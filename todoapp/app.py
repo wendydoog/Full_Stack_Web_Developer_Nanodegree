9f0c36b75ff910745a04407364bf06d41440e106
Siwen Wang <wendydoog@gmail.com>
2021-05-02 15:48:54 -0500

    update todo app

--- todoapp/app.py
+++ todoapp/app.py
@@ -5,6 +5,7 @@ import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://siwenwang@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 migrate = Migrate(app,db)
@@ -14,10 +15,18 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String(), nullable = False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
+
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-#db.create_all()
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(),nullable=False)
+    todos = db.relationship('Todo', backref = 'list', lazy=True)
+
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -25,7 +34,8 @@ def create_todo():
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        list_id = request.get_json()['list_id']
+        todo = Todo(description=description, complete=False, list_id=list_id)
         db.session.add(todo)
         db.session.commit() 
         body['description'] = todo.description
@@ -71,7 +81,13 @@ def delete_todo(todo_id):
 
 
 
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', 
+    data = Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    )
+
 @app.route('/')
 def index():
-    return render_template('index.html', data = Todo.query.order_by('id').all()
-    )
+    return redirect(url_for('get_list_todos', list_id = 1))
