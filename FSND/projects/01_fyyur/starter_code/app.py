fd4ec4754c430d7225537c2f38cc451c36c12b5f
Siwen Wang <wendydoog@gmail.com>
2021-05-16 19:41:30 -0500

    update fyyur

--- FSND/projects/01_fyyur/starter_code/app.py
+++ FSND/projects/01_fyyur/starter_code/app.py
@@ -294,7 +294,7 @@ def create_venue_submission():
   return render_template('pages/home.html')
 
 
-@app.route('/venues/<venue_id>', methods=['DELETE'])
+@app.route('/venues/<venue_id>/delete', methods=['DELETE'])
 def delete_venue(venue_id):
   # TODO: Complete this endpoint for taking a venue_id, and using
   # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
@@ -487,20 +487,22 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local venue to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
+  venue = Venue.query.get(venue_id)
+
+  if venue:
+    form.name.data = venue.name
+    form.genres.data = venue.genres
+    form.city.data = venue.city
+    form.state.data = venue.state
+    form.address.data = venue.address
+    form.phone.data = venue.phone
+    form.website_link.data = venue.website
+    form.facebook_link.data = venue.facebook_link
+    form.image_link.data = venue.image_link
+    form.seeking_talent.data = venue.seeking_talent
+    form.seeking_description.data = venue.seeking_description
+
+  
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue = venue)
 
@@ -508,7 +510,44 @@ def edit_venue(venue_id):
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+  
+  venue = Venue.query.get(venue_id)
+
+  try: 
+    if venue:
+      if 'seeking_talent' in request.form:
+        seeking_talent = True
+      else:
+        seeking_talent = False
+    venue.name = request.form['name']
+    venue.genres = request.form.getlist('genres')
+    venue.city = request.form['city']
+    venue.state = request.form['state']
+    venue.address = request.form['address']
+    venue.phone = request.form['phone']
+    venue.website = request.form['website_link']
+    venue.facebook_link = request.form['facebook_link']
+    venue.image_link = request.form['image_link']
+    venue.seeking_talent = seeking_talent
+    venue.seeking_description = request.form['seeking_description']
+    db.session.commit()
+
+  except:
+    db.session.rollback()
+    print(sys.exc_info())
+    flash('An error occurred. Venue ' + request.form['name'] + ' could not be edited.')
+    
+  finally:
+    db.session.close()
+    flash('Venue ' + request.form['name'] + ' was successfully edited!')
+
   return redirect(url_for('show_venue', venue_id=venue_id))
+
+
+
+
+
+
 
 #  Create Artist
 #  ----------------------------------------------------------------
