12a26c9cbea79b274e7b09e0b8b07164892737a9
Siwen Wang <wendydoog@gmail.com>
2021-05-29 11:38:03 -0500

    update

--- FSND/projects/02_trivia_api/starter/backend/test_flaskr.py
+++ FSND/projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -34,6 +34,27 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(len(data['categories']))
+
+    def test_404_sent_requesting_questions_beyond_valid_page(self):
+        res = self.client().get('/question?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    
+
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
